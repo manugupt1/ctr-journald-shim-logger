e45f1d697017d6188e46ae359a674036e0477099
Manu Gupta <manugupt1@gmail.com>
2022-09-10 20:11:44 -0700

    more logs

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
+	"path/filepath"
 	"sync"
 
 	"github.com/containerd/containerd/runtime/v2/logging"
-	"github.com/coreos/go-systemd/journal"
 )
 
 func main() {
@@ -17,14 +18,11 @@ func main() {
 
 func log(ctx context.Context, config *logging.Config, ready func() error) error {
 	// construct any log metadata for the container
-	vars := map[string]string{
-		"SYSLOG_IDENTIFIER": fmt.Sprintf("%s:%s", config.Namespace, config.ID),
-	}
 	var wg sync.WaitGroup
 	wg.Add(2)
-	// forward both stdout and stderr to the journal
-	go copy(&wg, config.Stdout, journal.PriInfo, vars)
-	go copy(&wg, config.Stderr, journal.PriErr, vars)
+	// forward both stdout and stderr to temp files
+	go copy(&wg, config.Stdout, config.ID, "stdout")
+	go copy(&wg, config.Stderr, config.ID, "stderr")
 
 	// signal that we are ready and setup for the container to be started
 	if err := ready(); err != nil {
@@ -34,10 +32,12 @@ func log(ctx context.Context, config *logging.Config, ready func() error) error 
 	return nil
 }
 
-func copy(wg *sync.WaitGroup, r io.Reader, pri journal.Priority, vars map[string]string) {
+func copy(wg *sync.WaitGroup, r io.Reader, id string, kind string) {
+	f, _ := os.Create(filepath.Join(os.TempDir(), fmt.Sprintf("%s_%s_%s.log", os.TempDir(), id, kind)))
+	defer f.Close()
 	defer wg.Done()
 	s := bufio.NewScanner(r)
 	for s.Scan() {
-		journal.Send(s.Text(), pri, vars)
+		f.WriteString(s.Text())
 	}
 }
